ee1eb099dad9d573e6b188ea34757005b75afbf2
Ryan <sailantht@gmail.com>
2022-02-18 09:51:32 +0330

    Video Proxy

--- videos/admin.py
+++ videos/admin.py
@@ -1,4 +1,29 @@
 from django.contrib import admin
-from .models import Video
+from .models import VideoPublishedProxy, VideoAllProxy
 
-admin.site.register(Video)
+
+class VideoAllAdmin(admin.ModelAdmin):
+    """
+        All videos
+    """
+    list_display = ["title", "video_id"]
+    search_fields = ['title']
+
+    class Meta:
+        model = VideoAllProxy
+
+
+class VideoProxyAdmin(admin.ModelAdmin):
+    list_display = ["title", "video_id"]
+    search_fields = ['title']
+
+    class Meta:
+        model = VideoPublishedProxy
+
+    def get_queryset(self, request):
+        return VideoPublishedProxy.objects.filter(active=True)
+    # so here we will have only published videos and all videos in the video admin
+
+
+admin.site.register(VideoAllProxy, VideoAllAdmin)
+admin.site.register(VideoPublishedProxy, VideoProxyAdmin)
